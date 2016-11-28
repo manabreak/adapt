29d4aaedee7a8914e3e87c92ddfc1fff38b724fe
manabreak <morbidi.h@gmail.com>
2016-11-28 18:56:26 +0200

    Added Javadoc

--- adapt/src/main/java/me/manabreak/adapt/Adapt.java
+++ adapt/src/main/java/me/manabreak/adapt/Adapt.java
@@ -16,73 +16,149 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * An easy-to-use adapter which supports multiple item types out of the box.
+ */
 public class Adapt extends RecyclerView.Adapter<Adapt.ViewHolder> {
 
     private final List<Object> items = new ArrayList<Object>();
     private final SparseArray<Class> bindRules = new SparseArray<>();
     private final Map<Class, Integer> typesToLayouts = new HashMap<>();
 
+    /**
+     * Adds a new type to this adapter.
+     * <p>
+     * This method basically tells the adapter to "use this layout
+     * when this kind of object is bound, using these rules."
+     *
+     * @param layout for the item
+     * @param clazz  type of the item
+     * @param rule   type of the rule class
+     * @param <R>    Type of the rule class
+     * @param <T>    Type of the item
+     * @return This adapter for chaining
+     */
     public <R extends BindRule<T>, T> Adapt addType(@LayoutRes int layout, @NonNull Class<T> clazz, @NonNull Class<R> rule) {
         bindRules.put(layout, rule);
         typesToLayouts.put(clazz, layout);
         return this;
     }
 
-    @SuppressWarnings("unchecked")
+    /**
+     * Constructs a new view holder.
+     *
+     * @param parent   of the view
+     * @param viewType layout used for the view
+     * @return A newly created view holder
+     */
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Class ruleClass = bindRules.get(viewType);
         if (ruleClass != null) {
             View itemView = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+            //noinspection unchecked
             return new ViewHolder<>(itemView, ruleClass);
         }
         throw new IllegalStateException("No rules added for viewType " + viewType);
     }
 
+    /**
+     * Binds the item at the given position to the given view holder.
+     *
+     * @param holder   to bind the item to
+     * @param position of the item
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        //noinspection unchecked
         holder.bind(items.get(position));
     }
 
+    /**
+     * Retrieves the total count of items.
+     *
+     * @return numer of items
+     */
     @Override
     public int getItemCount() {
         return items.size();
     }
 
+    /**
+     * Retrieves the registered layout for the item at 'position'.
+     *
+     * @param position of the item
+     * @return layout for the item
+     */
     @Override
     public int getItemViewType(int position) {
         return typesToLayouts.get(items.get(position).getClass());
     }
 
+    /**
+     * Adds a single item to this adapter.
+     *
+     * @param item to add
+     */
     public void add(@NonNull Object item) {
         //noinspection unchecked
         items.add(item);
     }
 
+    /**
+     * Adds all the items to this adapter.
+     *
+     * @param items to add
+     */
     public void add(@NonNull Object... items) {
         Collections.addAll(this.items, items);
     }
 
+    /**
+     * Adds all the items from the given collection to this adapter.
+     *
+     * @param items to add
+     */
     public void add(@NonNull Collection<?> items) {
         //noinspection unchecked
         this.items.addAll(items);
     }
 
+    /**
+     * Clears all the items added to this adapter.
+     */
     public void clear() {
         items.clear();
     }
 
+    /**
+     * Sets the items this adapter holds to the given items.
+     *
+     * @param items to set
+     */
     public void set(@NonNull Object... items) {
         this.items.clear();
         Collections.addAll(this.items, items);
     }
 
+    /**
+     * Sets the items this adapter holds to the items held by the given collection.
+     *
+     * @param items to set
+     */
     public void set(@NonNull Collection<?> items) {
         this.items.clear();
         //noinspection unchecked
         this.items.addAll(items);
     }
 
+    /**
+     * The single view holder class used with this adapter.
+     *
+     * @param <R> Type of the BindRule
+     * @param <T> Type of the item
+     */
     class ViewHolder<R extends BindRule<T>, T> extends RecyclerView.ViewHolder {
 
         private final R rule;
@@ -108,6 +184,13 @@ public class Adapt extends RecyclerView.Adapter<Adapt.ViewHolder> {
             rule.bind(item);
         }
 
+        /**
+         * Retrieves the BindRule associated with this view holder.
+         * <p>
+         * Should only be called from the tests.
+         *
+         * @return The BindRule set for this view holder
+         */
         @VisibleForTesting
         R getBindRule() {
             return rule;
--- adapt/src/main/java/me/manabreak/adapt/BindRule.java
+++ adapt/src/main/java/me/manabreak/adapt/BindRule.java
@@ -3,15 +3,47 @@ package me.manabreak.adapt;
 import android.support.annotation.NonNull;
 import android.view.View;
 
+/**
+ * A base class for all the bind rules.
+ * <p>
+ * A bind rule declares how to populate the given layout
+ * with the given item.
+ *
+ * @param <T> Type of the item
+ */
 public abstract class BindRule<T> {
 
+    /**
+     * The 'root' view of the layout
+     */
     @NonNull protected View itemView;
 
+    /**
+     * Sets the item view; called internally.
+     *
+     * @param itemView to set as the root view
+     */
     void setItemView(@NonNull View itemView) {
         this.itemView = itemView;
     }
 
+    /**
+     * Invoked once when the view holder is created.
+     * <p>
+     * This method should be used to initialize the
+     * views in the bind rule, as well as any other
+     * initializations that should be done just once.
+     */
     public abstract void init();
 
+    /**
+     * Invoked whenever a new item is bound to the
+     * view holder.
+     * <p>
+     * This method should be used to set the views
+     * accordingly.
+     *
+     * @param item to bind
+     */
     public abstract void bind(@NonNull T item);
 }
